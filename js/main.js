2c6efdea8de45fceee36b517ee53e8b148b01248
erynjones <eryn.jones16@hotmail.com>
2016-04-04 17:59:19 -0400

    trying to make it work

--- js/main.js
+++ js/main.js
@@ -7,5 +7,5 @@ $btn.on('click', function () {
   $lines.addClass('line2');
   $lines.removeClass('lines');
   $engage.addClass('engaged');
-  $(this).toggleClass('btn:active');
+  $(this).toggleClass('button');
 });
